7c0c8713b25184fc6b7604b92ceac6960db8afd4
JesperKauppinen <jesperkauppinen@gmail.com>
2022-03-20 20:44:01 +0200

    bullets added

---
+++ bullet.py
@@ -0,0 +1,25 @@
+import arcade
+from constants import BULLET_SCALING
+
+
+class Bullet(arcade.Sprite):
+    """ Player Sprite """
+    def __init__(self, hit_box_algorithm):
+        # Let parent initialize
+        super().__init__()
+
+        self.current_speed = 0
+        self.SPEED = 5
+
+        # Set our scale
+        self.scale = BULLET_SCALING
+
+        # load player texture
+        base_path = "resources/images/"
+        self.idle_texture_pair = arcade.load_texture_pair(f"{base_path}bullet.png", hit_box_algorithm=hit_box_algorithm)
+
+        # Set the initial texture
+        self.texture = self.idle_texture_pair[0]
+
+        # Hit box will be set based on the first image used.
+        self.hit_box = self.texture.hit_box_points
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arcade
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
 from player import Player
+from bullet import Bullet
 
 
 class Game(arcade.Window):
@@ -16,13 +17,16 @@ class Game(arcade.Window):
         # These are 'lists' that keep track of our sprites. Each sprite should
         # go into a list.
         self.player_list = None
+        self.bullet_list = None
 
         # Separate variable that holds the player sprite
         self.player = None
+        self.bullet = None
 
         # What key is pressed down?
         self.left_key_down = False
         self.right_key_down = False
+        self.space_down = False
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
@@ -31,6 +35,7 @@ class Game(arcade.Window):
 
         # Create the sprite lists
         self.player_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         # Create player sprite
         self.player = Player(hit_box_algorithm="Detailed")
@@ -53,9 +58,24 @@ class Game(arcade.Window):
 
         # Draw our sprites
         self.player_list.draw()
+        self.bullet_list.draw()
 
+    # Run every tick
+    # TODO: How to limit fps? Does computing power affect the speed?
     def on_update(self, delta_time: float):
+
+        # MOVE PLAYER: Add player y coordinate the current speed
         self.player.center_y += self.player.current_speed
+
+        # Cycle trough all bullets
+        for bullet in self.bullet_list:
+
+            # Move all Bullets Forwards
+            bullet.center_x += bullet.SPEED
+
+            # Check if bullet is in view, if not delete it
+            if bullet.center_x - bullet.width / 2 > SCREEN_WIDTH:
+                self.bullet_list.remove(bullet)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
@@ -69,6 +89,11 @@ class Game(arcade.Window):
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_key_down = True
             self.update_player_speed()
+
+        # Space
+        if key == arcade.key.SPACE:
+            self.space_down = True
+            self.shoot()
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
@@ -88,6 +113,20 @@ class Game(arcade.Window):
         # A pressed
         elif self.right_key_down and not self.left_key_down:
             self.player.current_speed = -self.player.SPEED
+
+    def shoot(self):
+        print("shot")
+        self.bullet = Bullet(hit_box_algorithm="Detailed")
+
+        # Set bullet location
+        self.bullet.center_x = self.player.center_x + self.player.width
+        self.bullet.center_y = self.player.center_y
+
+        # Turn the player -90 degree
+        # self.bullet.angle = 0
+
+        # Add to player sprite list
+        self.bullet_list.append(self.bullet)
 
 
 def main():
