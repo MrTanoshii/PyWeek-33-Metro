f8183329fea000897f31059ed19a44a2a38c4a03
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-03-21 19:37:42 +0400

    refactor: bullet collision moved to gameview

--- gameview.py
+++ gameview.py
@@ -7,13 +7,13 @@ import arcade
 import random
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, level1
 
+
 class GameView(arcade.View):
     """
     Main application class.
     """
 
     def __init__(self):
-
         # Call the parent class and set up the window
         super().__init__()
 
@@ -34,6 +34,8 @@ class GameView(arcade.View):
         # GUI
         self.score = 0
         self.gui_camera = None
+
+        self.setup_complete = False
 
         arcade.set_background_color(arcade.csscolor.DARK_GREEN)
 
@@ -75,8 +77,10 @@ class GameView(arcade.View):
         self.bg_list.draw()
         self.player_list.draw()
         Enemy.enemy_list.draw()
-        Bullet.bullet_list.draw()
+        Bullet.friendly_bullet_list.draw()
+        Bullet.enemy_bullet_list.draw()
 
+        # GUI - Score
         arcade.draw_text(
             f"Score : {self.score}",
             SCREEN_WIDTH / 5,
@@ -86,9 +90,19 @@ class GameView(arcade.View):
             anchor_x="center",
         )
 
+        # GUI - Player HP
+        arcade.draw_text(
+            f"HP : {self.player.cur_health}",
+            (SCREEN_WIDTH / 5) + 200,
+            SCREEN_HEIGHT - 50,
+            arcade.color.BLACK,
+            font_size=30,
+            anchor_x="center",
+        )
 
     # Run every tick
     # TODO: How to limit fps? Does computing power affect the speed?
+
     def on_update(self, delta_time: float):
         if random.randint(0, 200) == 1:
             self.spawn_bg()
@@ -105,10 +119,16 @@ class GameView(arcade.View):
 
         # MOVE PLAYER: Add player y coordinate the current speed
         self.player.center_y += self.player.current_speed
-        self.score += Bullet.update()
+        self.check_collisions()
 
         Enemy.update()
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        """Called whenever a mouse key is pressed."""
+        # Mouse Left Click
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            for enemy in Enemy.enemy_list:
+                enemy.shoot(Bullet.enemy_bullet_list)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
@@ -124,16 +144,16 @@ class GameView(arcade.View):
             self.update_player_speed()
 
         # Space
-        if key == arcade.key.SPACE:
+        elif key == arcade.key.SPACE:
             self.space_down = True
-            self.player.shoot(Bullet.bullet_list)
+            self.player.shoot(Bullet.friendly_bullet_list)
 
         # E
-        if key == arcade.key.E:
+        elif key == arcade.key.E:
             Enemy.spawn_enemy()
 
         # T
-        if key == arcade.key.T:
+        elif key == arcade.key.T:
             self.spawn_bg()
 
     def on_key_release(self, key, modifiers):
@@ -155,19 +175,61 @@ class GameView(arcade.View):
         elif self.right_key_down and not self.left_key_down:
             self.player.current_speed = -self.player.SPEED
 
-
-
     def spawn_bg(self):
         # Create BG sprite
         assets = level1.assets
-        self.bg = BackGround(asset=random.choice(assets), size=random.uniform(.9, 1.1))
+        self.bg = BackGround(asset=random.choice(
+            assets), size=random.uniform(.9, 1.1))
         self.bg.center_x = SCREEN_WIDTH + self.bg.width / 2
-        if random.randint(0,1) == 1:
-            self.bg.center_y = SCREEN_HEIGHT + self.bg.height / 2 - random.uniform(50, 75)
+        if random.randint(0, 1) == 1:
+            self.bg.center_y = SCREEN_HEIGHT + \
+                self.bg.height / 2 - random.uniform(50, 75)
         else:
             self.bg.center_y = self.bg.height / 2 + random.uniform(-50, 50)
         self.bg_list.append(self.bg)
 
+    def check_collisions(self):
+        """Check for collisions and calculate score"""
+        # Check friendly bullet collisions
+        for bullet in Bullet.friendly_bullet_list:
+            # Move all Bullets Forwards
+            bullet.center_x += bullet.SPEED
+
+            """ Collision """
+            # Add enemy to list, if collided with bullet
+            enemy_hit_list = arcade.check_for_collision_with_list(
+                bullet, Enemy.enemy_list
+            )
+
+            # Loop through each enemy we hit (if any) and remove it
+            for _enemy in enemy_hit_list:
+
+                # Remove bullet damage from enemy HP
+                _enemy.HP -= bullet.damage_value
+
+                # Remove bullet
+                bullet.remove_from_sprite_lists()
+
+                # if HP 0, destroy enemy
+                if _enemy.HP <= 0:
+                    _enemy.remove_from_sprite_lists()
+                    # Play a sound
+                    arcade.play_sound(_enemy.audio_destroyed)
+                    self.score += 1
+                else:
+                    # Play a sound
+                    arcade.play_sound(_enemy.audio_hit)
+        # Check enemy bullet collisions
+        for bullet in Bullet.enemy_bullet_list:
+            # Move all Bullets Forwards
+            bullet.center_x += bullet.SPEED
+
+            # Loop through each coin we hit (if any) and remove it
+            if arcade.check_for_collision(self.player, bullet):
+                # Apply damage to player
+                self.player.take_damage(bullet)
+                # Remove bullet
+                bullet.remove_from_sprite_lists()
+
     def on_show(self):
         self.setup()
-
