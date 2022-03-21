5cc198f71dd6bd96e94dab8f23e72ba5fd40089a
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-03-21 21:38:36 +0400

    fix: remove division by zero

--- lib.py
+++ lib.py
@@ -3,6 +3,9 @@ import math
 
 def calculate_angle(origin_x, origin_y, target_x, target_y):
     """Calculates the angle between 2 points, result is in radians"""
+    denom = origin_x - target_x
+    if denom == 0:
+        denom = 0.001
     angle = math.degrees(math.atan((origin_y - target_y) /
-                                   (origin_x - target_x)))
+                                   (denom)))
     return angle
