9690f1553a5edf2cd73c01f04d3e7e63702b46a2
KJ <irateboolean@gmx.com>
2022-03-22 12:17:46 -0400

    named cursor variable, added load_monuments

--- mapview.py
+++ mapview.py
@@ -1,6 +1,12 @@
-import arcade
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CENTER_POINTS
 import gameview
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CENTER_POINTS, LIST1
+import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CENTER_POINTS
+import arcade
+<< << << < HEAD
+== == == =
+
+>>>>>> > 5bb82e4(named cursor variable, added load_monuments)
 
 
 class MapView(arcade.View):
@@ -16,17 +22,14 @@ class MapView(arcade.View):
 
         # These are 'lists' that keep track of our sprites. Each sprite should
         # go into a list.
-        self.player_sprite = None
+        self.cursor_sprite = None
         self.player_list = None
 
         self.background = None
 
         self.monument_list = None
-        self.monument_sprite = None
 
         # arcade.set_background_color((170,218,255))
-
-        # self.set_mouse_visible(False)
 
     def setup(self):
         """ Set up everything with the game """
@@ -38,26 +41,24 @@ class MapView(arcade.View):
         self.background = arcade.load_texture(
             "resources/images/pixel_map.png")
 
-        self.player_sprite = arcade.Sprite(
+        self.cursor_sprite = arcade.Sprite(
             ":resources:images/animated_characters/female_person/femalePerson_idle.png", 0.5)
 
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
-        # First sprite
-        self.monument_sprite = arcade.Sprite(
-            "resources/images/pyramids.jpeg", 0.5)
-        self.monument_sprite.center_x = CENTER_POINTS[0][0]
-        self.monument_sprite.center_y = CENTER_POINTS[0][1]
-        self.monument_sprite.name = "EGYPT"
-        self.monument_list.append(self.monument_sprite)
+        self.cursor_sprite.center_x = 50
+        self.cursor_sprite.center_y = 50
+        self.player_list.append(self.cursor_sprite)
+        self.load_monuments()
 
-        self.monument_sprite = arcade.Sprite(
-            "resources/images/taj_mahal.jpeg", 0.5)
-        self.monument_sprite.center_x = CENTER_POINTS[1][0]
-        self.monument_sprite.center_y = CENTER_POINTS[1][1]
-        self.monument_sprite.name = "INDIA"
-        self.monument_list.append(self.monument_sprite)
+    def load_monuments(self):
+        """Loads map monuments onto map"""
+        for monument in constants.MAP_MONUMENTS:
+            monument_sprite = arcade.Sprite(
+                "resources/images/" +
+                constants.MAP_MONUMENTS[monument]["img_name"],
+                constants.MAP_MONUMENTS[monument]["scale"])
+            monument_sprite.center_x = constants.MAP_MONUMENTS[monument]["center_x"]
+            monument_sprite.center_y = constants.MAP_MONUMENTS[monument]["center_y"]
+            self.monument_list.append(monument_sprite)
 
     def on_draw(self):
         """Render the screen."""
@@ -75,14 +76,13 @@ class MapView(arcade.View):
     def on_mouse_motion(self, x, y, dx, dy):
         for monument in self.monument_list:
             monument.scale = 0.5
-        self.player_sprite.center_x = x+20
-        self.player_sprite.center_y = y-20
+        self.cursor_sprite.center_x = x+20
+        self.cursor_sprite.center_y = y-20
 
     def on_update(self, delta_time):
-        self.monument_sprite.update()
 
         hit_list = arcade.check_for_collision_with_list(
-            self.player_sprite, self.monument_list)
+            self.cursor_sprite, self.monument_list)
 
         for i in hit_list:
             i.scale = 0.7
@@ -90,14 +90,13 @@ class MapView(arcade.View):
         # self.monument_sprite.clear()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        p = self.player_sprite.collides_with_list(self.monument_list)
+        p = self.cursor_sprite.collides_with_list(self.monument_list)
         for location in p:
-            if location.name == "EGYPT":
-                self.window.show_view(gameview.GameView())
-            elif location.name == "INDIA":
-                self.window.show_view(gameview.GameView())
+            self.window.show_view(gameview.GameView())
+
 
 # Make center points as dictionary and call out other views mostly
+
 
     def on_show(self):
         self.setup()
