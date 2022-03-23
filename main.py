ab4a7ba08f67051b2c2a186a4c3d91f600d181c4
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-03-23 11:42:48 +0400

    feat: Center game window

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import arcade
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN, CURSOR_VISIBLE
+import constants as C
 from mainmenu import MainMenu
 
 
 def main():
     """Main function"""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN)
-    window.set_mouse_visible(CURSOR_VISIBLE)
+    window = arcade.Window(C.SCREEN_WIDTH, C.SCREEN_HEIGHT,
+                           C.SCREEN_TITLE, C.FULLSCREEN, center_window=C.CENTER_WINDOW)
+    window.set_mouse_visible(C.CURSOR_VISIBLE)
     menu_view = MainMenu()
     window.show_view(menu_view)
     arcade.run()
