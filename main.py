24f1ca2219c39771142fb2c35fb5a1a05c6485e7
JesperKauppinen <jesperkauppinen@gmail.com>
2022-03-23 02:39:02 +0200

    cursor invisible

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import arcade
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN, CURSOR_VISIBLE
 from mainmenu import MainMenu
 
 
 def main():
     """Main function"""
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN)
+    window.set_mouse_visible(CURSOR_VISIBLE)
     menu_view = MainMenu()
     window.show_view(menu_view)
     arcade.run()
