bc19d871c12055fdd0e611e4787086e0f167e223
JesperKauppinen <jesperkauppinen@gmail.com>
2022-03-23 02:38:28 +0200

    fullscreen option

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import arcade
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN
 from mainmenu import MainMenu
 
 
 def main():
     """Main function"""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, FULLSCREEN)
     menu_view = MainMenu()
     window.show_view(menu_view)
     arcade.run()
