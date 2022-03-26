fb3770c80f047fe73772c0554edab9e0acb05ccd
JesperKauppinen <jesperkauppinen@gmail.com>
2022-03-26 17:49:29 +0200

    base for story

---
+++ const/story.py
@@ -0,0 +1,15 @@
+STORY = [
+    # Story 1
+    [
+      ["FSM: Hello Man, I have a task for u"],
+      ["MAN: who the F' are you lmao :D"],
+      ["FSM: is decidawdded that u do thdawis and "],
+    ],
+    # Story 2
+    [
+      ["** angry goat noices ** "],
+      ["FSM: Next you are assigned to ..."],
+      ["MAN: no what no lol why me"],
+      ["FSM: is decidawdded that u do thdawis and "],
+    ],
+]
