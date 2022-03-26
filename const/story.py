e58a148c6674dfabe3053613a6d860eaff5a9fb2
RGP <breepromo@gmail.com>
2022-03-26 17:26:15 +0000

    created story game dialogue first version

--- const/story.py
+++ const/story.py
@@ -1,39 +1,100 @@
 STORY = [
-    # Story 1
+    # Story 1 (before level 1)
     [
-      ["FSM: Hello Man, I have a task for u"],
-      ["MAN: who the F' are you lmao :D"],
-      ["FSM: is decidawdded that u do thdawis and "],
+        ["MAN: Good morning G.O.A.T, did you sleep well?"],
+        ["** GOAT MUNCHING NOISE **"],
+        ["MAN: I trust we will have another busy day ahead of us."],
+        ["** GOAT MUNCHING NOISE **"],
+        ["MAN: Sometimes I wish there was more to life but alas I follow in the family business."],
+        ["** GOAT ALARMED NOISE **"],
+        ["MAN: What is it my friend? Is that a flying object?! Is it a new tezla satellite?!"],
+        ["FSM: Greetings chosen one, I am the Flying Spaghetti Monster!"],
+        ["MAN: WTF?!"],
+        ["FSM: I come from a far away place and sense all is not well with your world."],
+        ["MAN: This cannot be real, I think I drank one too many sorghums last night!"],
+        ["** GOAT AGREEING NOISE **"],
+        ["FSM: You are not dreaming, I need you and your G.O.A.T to fight and save the people,"],
+        ["FSM: at various cities across the world there has been an uprising of evil twins,"],
+        ["FSM: they have captured the good twin leaders and are causing havoc and chaos,"],
+        ["FSM: as a 17th generation GOAT herder I sense you are more than capable of fighting for the greater good,"],
+        ["FSM: and you look like the sort of man who can get his hands dirty - plus I know G.O.A.T can kick ass!"],
+        ["MAN: I don't know, this is a great shock, it does not seem real, it seems like a made up story of BS!"],
+        ["MAN: I am not a fighter, well not really, and my friend here is a passive, easy going mountain animal!"],
+        ["FSM: Well, I am confident in your abilities and I will help you along the way, the people need you,"],
+        ["FSM: I think you should give it go, what do you say?"],
+        ["MAN: Mmm... I do like the idea of fighting evil and you said you can help?"],
+        ["FSM: Yes, I will use my spaghetti powers to provide weapons and vehicles, you will get the hang of it fast."],
+        ["MAN: OK, I will ask my friend; 'G.O.A.T what do you think? Shall we mix things up a little?'"],
+        ["** GOAT AGREEING NOISE **"],
+        ["MAN: The answer is yes, let's do this!"],
+        ["FSM: OK, the first city is Ayyjeep. Take this gun, defeat the bad guys and kill the evil twin!"],
+        ["FSM: It will help if you collect some gold coin along the way and score over 100 points, good luck!"],
     ],
-    # Story 2
+    # Story 2 (after completing level 1, before level 2)
     [
-      ["** angry goat noices ** "],
-      ["FSM: Next you are assigned to ..."],
-      ["MAN: no what no lol why me"],
-      ["FSM: is decidawdded that u do thdawis and "],
+        ["FSM: Well done, you conquered the first city! You did well and now it is time to move north,"],
+        ["FSM: there is an evil twin operating out of Rushaa, head over there now and take care of business!"],
+        ["MAN: OK! We will get there ASAP, it is long way for us to travel and I think G.O.A.T's feet need a rest,"],
+        ["MAN: can you do anything to assist with our mode of transport?"],
+        ["FSM: Yes of course, you both deserve an upgrade! Take this jeep and take down the evil twin!"],
+        ["FSM: They are a tough bunch but I am confident in your abilities,"],
+        ["FSM: remember to pick up as much gold as you can and get over 100 points!"],
     ],
-    # Story 3
+    # Story 3 (after completing level 2, before level 3)
     [
-      [""],
+        ["MAN: Wow! We did it, that was tough going, you fight well my friend."],
+        ["** GOAT AGREEING NOISE **"],
+        ["MAN: We picked up some gold and conquered another city, shall we now have a break?"],
+        ["FSM: There is no time for a break Mr Herder, another evil twin has a strong hold in Shyna!"],
+        ["FSM: Be quick and move east!"],
     ],
-    # Story 2
+    # Story 4 (after completing level 3, before level 4)
     [
-        [""],
+        ["FSM: Impressive work! You are proving to be a formidable tag team, well done!"],
+        ["MAN: Thank you, now we can take our rest and put our feet up,"],
+        ["MAN: I hear that they do a lovely Kung Po chicken down the road! Are you hungry G.O.A.T?"],
+        ["** GOAT AGREEING NOISE **"],
+        ["FSM: Your rest will come later. There is no time to lose, another evil twin has set up on the ocean,"],
+        ["FSM: get back in the motor and drive to the end of road and get out to sea!"],
+        ["MAN: What?! In the water, G.O.A.T can't swim and I didn't pack my trunks! Can you help us out?"],
+        ["FSM: Indeed, I will get this sorted. Go now to the next area and I will get something ready for you,"],
+        ["FSM: I will have to be creative as I can't have you using all your energy on a rowing boat..."],
     ],
-    # Story 4
+
+    # Story 5 (after completing level 4, before level 5)
     [
-        [""],
+        ["MAN: That was awesome! We caught some big waves and defeated the bad guy!"],
+        ["** GOAT AGREEING NOISE **"],
+        ["FSM: I have to agree, that was totally cool!"],
+        ["FSM: Now I need you back on the road and over to South Hamerica, there is no time to lose!"],
+        ["MAN: OK ok, we know the drill by now. Can we at least take the surfboard until we reach the mainland?!"],
     ],
-    # Story 5
+    # Story 6 (after completing level 5, before level 6)
     [
-        [""],
+        ["MAN: That was awesome! We caught some big waves and defeated the bad guy!"],
+        ["** GOAT AGREEING NOISE **"],
+        ["FSM: I have to agree, that was totally cool!"],
+        ["FSM: Now I need you back on the road and over to South Hamerica, there is no time to lose!"],
+        ["MAN: OK ok, we know the drill by now. Can we at least take the surfboard until we reach the mainland?!"],
+        ["FSM: Yes but when you reach the city, it will be back on all fours, G.O.A.T get ready for more action! "],
     ],
-    # Story 6
+    # Story 7 (after completing level 6, before level 7)
     [
-        [""],
+        ["FSM: Fantastic work! I knew you were the right choice. You are doing very well!"],
+        ["FSM: There is now just one evil twin remaining, he has is setting up on the West Coast of Hamerica,"],
+        ["FSM: ride north until you reach the bright lights of a colourful spot called Viva Los Vaygus!"],
+        ["MAN: OK, we will get right on it won't we G.O.A.T?"],
+        ["** GOAT AGREEING NOISE **"],
+        ["MAN: We will put a stop to these evil twins once and for all!"],
+        ["MAN: Plus it will be a great place for us to chill out in and spend any gold coin we have left!"],
+        ["FSM: Good luck Mr Herder and G.O.A.T, I have a feeling you might need it on this one..."],
+
     ],
-    # Story 7
+    # Story 7 (game completed)
     [
-        [""],
+        ["FSM: You did it player, you saved the world! I am so proud of you!"],
+        ["FSM: The people thank you and you can now finally have some rest!"],
+        ["MAN: Excellent, that is good to hear! We have some gold coin left to spend,"],
+        ["MAN: I saw an all you can eat buffet and G.O.A.T wants to mingle with the high rollers!"],
     ],
 ]
