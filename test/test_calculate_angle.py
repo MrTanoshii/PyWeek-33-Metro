fc710879e062180592db17e5c924ef887fcdcd06
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-04-06 17:28:14 +0400

    test: add `calculate_angle` test

--- test/test_calculate_angle.py
+++ test/test_calculate_angle.py
@@ -8,6 +8,7 @@ class TestSum(unittest.TestCase):
     def test_calculate_angle(self):
         self.assertAlmostEqual(calculate_angle(0, 0, 0, 0), 0.00000, 5, "Should be 0.00000")
         self.assertAlmostEqual(calculate_angle(1, 2, 3, 4), 45.00000, 5, "Should be 45.00000")
+        self.assertAlmostEqual(calculate_angle(-1, -2, -3, -4), 45.00000, 5, "Should be 45.00000")
 
 
 if __name__ == '__main__':
